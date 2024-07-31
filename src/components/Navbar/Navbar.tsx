a029d6e73bb6a2e546ba6ccc1aa4f04b7d896adb
Aslan de Lyss <aslandelyss@gmail.com>
2024-07-31 18:58:22 +0200

    feat: creates navbar and sets its statics

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,32 @@
-
-import styles from "./Navbar.module.scss";
+import Image from 'next/image'
+import styles from './Navbar.module.scss'
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <ul>
-        <li>
-        MARVEL LOGO
-        </li>
-        <li>
-          LIKES
-        </li> 
-      </ul>
-    </nav>
-  );
-};
+	return (
+		<nav className={styles.navbar}>
+			<ul>
+				<li key={'marvelLogo'}>
+					<Image
+						src='/Marvel logo.svg'
+						alt='Marvel logo'
+						width={100}
+						height={50}
+					/>
+				</li>
+				<li key='heartIcon'>
+					<div>
+						<Image
+							src='/Heart icon.svg'
+							alt='Marvel logo'
+							width={24}
+							height={24}
+						/>
+						<span>0</span>
+					</div>
+				</li>
+			</ul>
+		</nav>
+	)
+}
 
-export default Navbar;
+export default Navbar
