2f25a5638da2929332475471f333809d31eeb02f
Aslan de Lyss <aslandelyss@gmail.com>
2024-07-31 19:01:09 +0200

    test: create char reducer test

---
+++ src/contexts/CharactersContext/tests/charactersReducer.test.ts
@@ -0,0 +1,30 @@
+import { describe, it } from 'node:test'
+
+describe('charactersReducer', () => {
+	it('should handle FETCH_START', () => {
+
+  })
+  it('should handle FETCH_SUCCESS', () => {
+    // Test adding new characters to state
+  });
+
+  it('should handle FETCH_ERROR', () => {
+    // Test error state
+  });
+
+  it('should handle SELECT_CHARACTER', () => {
+    // Test selecting a character
+  });
+
+  it('should handle CLEAR_SELECTION', () => {
+    // Test clearing selected character
+  });
+
+  it('should handle SET_SEARCH_TERM', () => {
+    // Test setting search term and resetting offset
+  });
+
+  it('should handle SET_HAS_MORE', () => {
+    // Test setting hasMore flag
+  });
+})
