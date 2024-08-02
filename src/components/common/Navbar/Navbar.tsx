59b20c182a4b5fb7cd81bd4d3e9df0bef58eb36a
Aslan de Lyss <aslandelyss@gmail.com>
2024-08-02 11:00:48 +0200

    chore: reorganize folders

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,29 +1,34 @@
 import Image from 'next/image'
 import styles from './Navbar.module.scss'
 import Link from 'next/link'
+import { useCharacters } from '@/hooks/useCharacters'
 
 const Navbar = () => {
+	const { toggleShowFavorites } = useCharacters()
+
 	return (
 		<nav className={styles.navbar}>
 			<ul>
 				<li key={'marvelLogo'}>
-					<Link href="/">
+					<Link href='/'>
 						<Image
-							src="/Marvel logo.svg"
-							alt="Marvel logo"
+							src='/Marvel logo.svg'
+							alt='Marvel logo'
 							width={100}
 							height={50}
 						/>
 					</Link>
 				</li>
-				<li key="heartIcon">
+				<li key='heartIcon'>
 					<div>
-						<Image
-							src="/Heart icon.svg"
-							alt="Marvel logo"
-							width={24}
-							height={24}
-						/>
+						<button onClick={toggleShowFavorites}>
+							<Image
+								src='/Heart icon.svg'
+								alt='Marvel logo'
+								width={24}
+								height={24}
+							/>
+						</button>
 						<span>0</span>
 					</div>
 				</li>
