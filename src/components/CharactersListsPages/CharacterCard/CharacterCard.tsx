3abb1e7d29e5bd78c1c6a84e849f038acfdadf80
Aslan de Lyss <aslandelyss@gmail.com>
2024-08-02 11:00:23 +0200

    feat: add favorite button to character card

--- src/components/CharactersListsPages/CharacterCard/CharacterCard.tsx
+++ src/components/CharactersListsPages/CharacterCard/CharacterCard.tsx
@@ -2,24 +2,30 @@ import Image from 'next/image'
 import styles from './CharacterCard.module.scss'
 import Link from 'next/link'
 import { useCharacters } from '@/hooks/useCharacters'
-import { Character } from '@/types/types'
+import FavoriteButton from '@/components/common/FavoriteButton/FavoriteButton'
+import { Character } from '@/contexts/CharactersContext/types/characterTypes'
 
 interface CharacterCardProps {
 	imageSrc: string
-  name: string
-  character: Character
+	name: string
+	character: Character
 }
 const CharacterCard = ({ imageSrc, name, character }: CharacterCardProps) => {
-  const {selectCharacter} = useCharacters()
-  const handleOnClickLink = () => {
-    selectCharacter(character)
-  }
+	const { selectCharacter } = useCharacters()
+
+	const handleOnClickLink = () => {
+		selectCharacter(character)
+	}
+
 	return (
 		<div className={styles.character_card}>
 			<Link href={`/character/${character.id}`} onClick={handleOnClickLink}>
-				<Image src={imageSrc} alt="Character image" width="190" height="190" />
-				<p>{name}</p>
+				<Image src={imageSrc} alt='Character image' width='190' height='190' />
 			</Link>
+				<div className={styles.info}>
+					<p>{name}</p>
+					<FavoriteButton character={character} />
+				</div>
 		</div>
 	)
 }
