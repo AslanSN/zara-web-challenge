adb117bbf6e41ef69a3f99241b2eeb168069f7f8
Aslan de Lyss <aslandelyss@gmail.com>
2024-08-03 13:05:47 +0200

    feat: add dynamic chars counting

--- src/components/CharactersListsPages/Searchbar/Searchbar.tsx
+++ src/components/CharactersListsPages/Searchbar/Searchbar.tsx
@@ -1,12 +1,22 @@
+import { useCharacters } from '@/hooks/useCharacters'
 import styles from './Searchbar.module.scss'
+import { ChangeEvent } from 'react'
 
 const Searchbar = () => {
+	const {searchCharacters, searchTerm, charactersDisplaying } = useCharacters()
+	const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+		searchCharacters(event.target.value)
+	}
+	
 	return (
 		<section className={styles.search_bar}>
-			<input type='text' placeholder='SEARCH A CHARACTER' />
-      <span>
-        Number of Items
-      </span>
+			<input
+				type='text'
+				placeholder='SEARCH A CHARACTER'
+				value={searchTerm}
+				onChange={handleOnChange}
+			/>
+			<span>{charactersDisplaying}</span>
 		</section>
 	)
 }
