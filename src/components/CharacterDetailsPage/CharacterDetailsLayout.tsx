21e0404f02653e6d7987f6e54d291a3686066766
Aslan de Lyss <aslandelyss@gmail.com>
2024-08-03 13:03:17 +0200

    chore: improve characters details logics

--- src/components/CharacterDetailsPage/CharacterDetailsLayout.tsx
+++ src/components/CharacterDetailsPage/CharacterDetailsLayout.tsx
@@ -2,28 +2,49 @@ import { useCharacters } from '@/hooks/useCharacters'
 import CharacterHeader from './CharacterHeader/CharacterHeader'
 import ComicsList from './ComicsList/ComicsList'
 import styles from './CharacterDetailsLayout.module.scss'
+import { useEffect } from 'react'
 const CharacterDetailsLayout = () => {
-	const { selectedCharacter, isLoading, error } = useCharacters()
+	const {
+		selectedCharacter,
+		comicsImages,
+		isLoading,
+		error,
+		fetchCharacterImages,
+	} = useCharacters()
+
+	useEffect(() => {
+		if (!selectedCharacter) return
+
+		const hasAlreadyImagePath = comicsImages.some(
+			(comic) => comic.id === selectedCharacter.id
+		)
+
+		if (hasAlreadyImagePath) return
+
+		fetchCharacterImages(selectedCharacter)
+	}, [comicsImages, fetchCharacterImages, selectedCharacter])
+
 	if (isLoading) return <h1>Loading...</h1>
-
 	if (!selectedCharacter) return <h1>No character selected</h1>
-
 	if (error) return <h1>{error}</h1>
+
+	const characterPath = selectedCharacter
+		? `${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`
+		: ''
+	const characterComics = comicsImages.find(
+		(comic) => comic.id === selectedCharacter.id
+	)
 
 	return (
 		<div className={styles.container}>
 			<CharacterHeader
 				name={selectedCharacter.name}
-				imageSrc={
-					selectedCharacter.thumbnail.path +
-					'.' +
-					selectedCharacter.thumbnail.extension
-				}
+				imageSrc={characterPath}
 				description={selectedCharacter.description}
+				character={selectedCharacter}
 			/>
 			<section className={styles.comics}>
-
-			<ComicsList comics={selectedCharacter.comics} />
+				<ComicsList comics={characterComics} />
 			</section>
 		</div>
 	)
