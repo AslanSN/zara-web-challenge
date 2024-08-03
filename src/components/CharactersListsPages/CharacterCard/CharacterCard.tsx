797134a4ddb9e21624f9c3e787e5a7e0270ebe67
AslanSN <aslan.stane@gmail.com>
2024-08-04 01:16:08 +0200

    chore: add styled components to char card

--- src/components/CharactersListsPages/CharacterCard/CharacterCard.tsx
+++ src/components/CharactersListsPages/CharacterCard/CharacterCard.tsx
@@ -1,31 +1,46 @@
 import Image from 'next/image'
 import styles from './CharacterCard.module.scss'
+import styled from 'styled-components'
 import Link from 'next/link'
 import { useCharacters } from '@/hooks/useCharacters'
 import FavoriteButton from '@/components/common/FavoriteButton/FavoriteButton'
 import { Character } from '@/contexts/CharactersContext/types/characterTypes'
+import useCharacterCardWidth from './hooks/useCharacterCardWidth'
+import { useEffect } from 'react'
 
 interface CharacterCardProps {
 	imageSrc: string
 	name: string
 	character: Character
 }
+
+const InfoText = styled.p<{ maxWidth: number }>`
+	max-width: ${(props) => props.maxWidth}px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	text-align: start;
+`
+
 const CharacterCard = ({ imageSrc, name, character }: CharacterCardProps) => {
 	const { selectCharacter } = useCharacters()
-
+	const imageWidth = useCharacterCardWidth()
 	const handleOnClickLink = () => {
 		selectCharacter(character)
 	}
-
 	return (
-		<div className={styles.character_card}>
+		<div className={`${styles.character_card} card`}>
 			<Link href={`/character/${character.id}`} onClick={handleOnClickLink}>
-				<Image src={imageSrc} alt='Character image' width='190' height='190' />
+				<Image
+					src={imageSrc}
+					alt="Character image"
+					height={190}
+					width={imageWidth}
+				/>
 			</Link>
-				<div className={styles.info}>
-					<p>{name}</p>
-					<FavoriteButton character={character} />
-				</div>
+			<div className={styles.info}>
+				<InfoText maxWidth={imageWidth - 50}>{name}</InfoText>
+				<FavoriteButton character={character} />
+			</div>
 		</div>
 	)
 }
