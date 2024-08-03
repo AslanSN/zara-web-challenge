cfb0ccda395cf2686f52a0d3a807332cf3aeaeb1
AslanSN <aslan.stane@gmail.com>
2024-08-04 01:16:38 +0200

    feat: add search icon and improve search bar styles

--- src/components/CharactersListsPages/Searchbar/Searchbar.tsx
+++ src/components/CharactersListsPages/Searchbar/Searchbar.tsx
@@ -1,22 +1,45 @@
 import { useCharacters } from '@/hooks/useCharacters'
 import styles from './Searchbar.module.scss'
 import { ChangeEvent } from 'react'
+import Image from 'next/image'
+
+const SubtitleText = () => {
+	const { isLoading, charactersDisplaying, limit, showFavorites } =
+		useCharacters()
+	if (isLoading && charactersDisplaying === 0) {
+		return showFavorites ? (
+			<span>Loading favorites...</span>
+		) : (
+			<span>looking for {limit} characters...</span>
+		)
+	}
+
+	return (
+		<span>{`${charactersDisplaying}	${
+			charactersDisplaying === 1 ? 'result' : 'results'
+		}`}</span>
+	)
+}
 
 const Searchbar = () => {
-	const {searchCharacters, searchTerm, charactersDisplaying } = useCharacters()
+	const { searchCharacters, searchTerm } = useCharacters()
 	const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
 		searchCharacters(event.target.value)
 	}
-	
+
 	return (
 		<section className={styles.search_bar}>
-			<input
-				type='text'
-				placeholder='SEARCH A CHARACTER'
-				value={searchTerm}
-				onChange={handleOnChange}
-			/>
-			<span>{charactersDisplaying}</span>
+			<div className={styles.input_container}>
+				<Image src="/searchIcon.svg" alt="Search icon" width={12} height={12} />
+				<input
+					type="text"
+					placeholder="search a character..."
+					value={searchTerm}
+					onChange={handleOnChange}
+				/>
+			</div>
+
+			<SubtitleText />
 		</section>
 	)
 }
