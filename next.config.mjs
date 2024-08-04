c391ac63f6b8733c0436e80e52d4b348739b468b
AslanSN <aslan.stane@gmail.com>
2024-08-04 20:33:10 +0200

    chore: optimize app build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	productionBrowserSourceMaps: false,
 	images: {
 		remotePatterns: [
 			{
@@ -10,6 +11,19 @@ const nextConfig = {
 				pathname: '/**',
 			},
 		],
+	},
+	webpack: (config, { dev, isServer }) => {
+		if (!dev && !isServer) {
+			config.optimization.splitChunks.cacheGroups = {
+				...config.optimization.splitChunks.cacheGroups,
+				commons: {
+					name: 'commons',
+					chunks: 'all',
+					minChunks: 2,
+				},
+			}
+		}
+		return config
 	},
 }
 export default nextConfig
