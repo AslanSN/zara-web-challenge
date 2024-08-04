994d1950811338507faecb3db045fe3157eaac0e
AslanSN <aslan.stane@gmail.com>
2024-08-04 21:00:30 +0200

    chore: improve characters list styles and logic

--- src/components/CharactersListsPages/CharactersList/CharactersList.tsx
+++ src/components/CharactersListsPages/CharactersList/CharactersList.tsx
@@ -1,9 +1,31 @@
 'use client'
 import CharacterCard from '../CharacterCard/CharacterCard'
-import { useCharacters } from '@/hooks/useCharacters'
+import { useCharacters } from '@/contexts/CharactersContext/hooks/useCharacters'
 import styles from './CharactersList.module.scss'
-import { useMemo } from 'react'
-import { isNotEmptyArray, isNotEmptySet } from '@/utils/predicates'
+import { useEffect, useMemo } from 'react'
+import {
+	isNotEmptyArray,
+	isNotEmptySet,
+} from '@/contexts/CharactersContext/utils/predicates'
+import styled from 'styled-components'
+
+// const CharactersListSkeleton = () => {
+// 	return (
+// 		<div className={styles.characters_list_skeleton}>
+// 			{[...Array(20)].map((_, index) => (
+// 				<div
+// 					key={index}
+// 					className={styles.character_card_skeleton}
+// 				></div>
+// 			))}
+// 		</div>
+// 	)
+// }
+
+const MessageTitle = styled.h3`
+	margin: 0 0 1.5rem 0;
+`
+
 const CharactersList = () => {
 	const {
 		allCharacters,
@@ -17,30 +39,38 @@ const CharactersList = () => {
 	} = useCharacters()
 
 	const hasSearchTerm = useMemo(() => searchTerm !== '', [searchTerm])
+	const { hasToDisplayFavorites, hasToDisplayFilteredCharacters } = useMemo(
+		() => ({
+			hasToDisplayFavorites:
+				showFavorites && isNotEmptyArray(favorites) && !hasSearchTerm,
+			hasToDisplayFilteredCharacters:
+				hasSearchTerm && isNotEmptySet(filteredCharacters),
+		}),
+		[favorites, filteredCharacters, hasSearchTerm, showFavorites]
+	)
 	const charactersToDisplay = useMemo(() => {
-		if (showFavorites && isNotEmptyArray(favorites) && !hasSearchTerm) {
-			setCharactersDisplaying(favorites.length)
+		if (hasToDisplayFavorites) {
 			return favorites
 		}
-		
-		if (hasSearchTerm && isNotEmptySet(filteredCharacters)) {
-			setCharactersDisplaying(filteredCharacters.size)
+
+		if (hasToDisplayFilteredCharacters) {
 			return Array.from(filteredCharacters)
 		}
-
-		setCharactersDisplaying(allCharacters.length)
 		return allCharacters
 	}, [
 		allCharacters,
 		favorites,
 		filteredCharacters,
-		hasSearchTerm,
-		setCharactersDisplaying,
-		showFavorites,
+		hasToDisplayFavorites,
+		hasToDisplayFilteredCharacters,
 	])
 
+	useEffect(() => {
+		setCharactersDisplaying(charactersToDisplay.length)
+	}, [charactersToDisplay.length, setCharactersDisplaying])
+
 	if (isLoading && allCharacters.length === 0) {
-		return <p>Loading...</p>
+		return showFavorites ? <p>Loading favorites...</p> : <p>Loading...</p>
 	}
 
 	if (error) {
@@ -50,10 +80,10 @@ const CharactersList = () => {
 	return (
 		<section className={styles.character_list}>
 			{showFavorites && !isNotEmptyArray(favorites) && (
-				<h3>You should add some favorites first</h3>
+				<MessageTitle>You should add some favorites first</MessageTitle>
 			)}
 			{hasSearchTerm && !isNotEmptySet(filteredCharacters) && (
-				<h3>No characters found</h3>
+				<MessageTitle>No characters found</MessageTitle>
 			)}
 			<ul>
 				{charactersToDisplay.map((character) => (
