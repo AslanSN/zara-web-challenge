002e3fb41cfb8e6b63b0044f6e6d55260645f8f2
AslanSN <aslan.stane@gmail.com>
2024-08-04 20:34:20 +0200

    chore: clean main page files

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import CharacterDetailsLayout from '@/components/CharacterDetailsPage/CharacterDetailsLayout'
-import { useCharacters } from '@/hooks/useCharacters'
+import { useCharacters } from '@/contexts/CharactersContext/hooks/useCharacters'
 import { useEffect } from 'react'
 
 const Character = ({ params }: { params: { id: string } }) => {
@@ -13,7 +13,7 @@ const Character = ({ params }: { params: { id: string } }) => {
 				!isLoading &&
 				(selectedCharacter === null ||
 					selectedCharacter?.id !== Number(params.id))
-				)
+			)
 				await fetchCharacter(Number(params.id))
 		}
 		loadCharacter()
