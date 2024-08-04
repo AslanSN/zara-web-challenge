af1420caa7ce1d23a4731e32705cc53b244b107e
AslanSN <aslan.stane@gmail.com>
2024-08-04 20:38:17 +0200

    chore: get local favorites

--- src/components/CharactersListsPages/HomePageContainer.tsx
+++ src/components/CharactersListsPages/HomePageContainer.tsx
@@ -1,19 +1,27 @@
 'use client'
-import { useCharacters } from '@/hooks/useCharacters'
+import { useCharacters } from '@/contexts/CharactersContext/hooks/useCharacters'
 import CharactersList from './CharactersList/CharactersList'
 import Searchbar from './Searchbar/Searchbar'
 import { useEffect } from 'react'
+import styles from './HomePageContainer.module.scss'
 
 export default function HomePageContainer() {
-	const { fetchNextPage } = useCharacters()
+	const { fetchNextPage, setFavorites, showFavorites } = useCharacters()
 	useEffect(() => {
 		fetchNextPage()
 	}, [fetchNextPage])
 
+	useEffect(() => {
+		const storedFavorites = localStorage.getItem('favorites')
+		if (storedFavorites) {
+			setFavorites(JSON.parse(storedFavorites))
+		}
+	}, [setFavorites])
 	return (
-		<>
+		<section className={styles.home_page_container}>
+			{showFavorites && <h2 className={styles.favorites_title}>Favorites</h2>}
 			<Searchbar />
 			<CharactersList />
-		</>
+		</section>
 	)
 }
