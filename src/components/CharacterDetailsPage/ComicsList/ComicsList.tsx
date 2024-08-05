ba24b6fa81208ae0d84275f916372d24fc7e65b5
Aslan de Lyss <aslandelyss@gmail.com>
2024-08-05 22:49:00 +0200

    chore: improve comics list management

--- src/components/CharacterDetailsPage/ComicsList/ComicsList.tsx
+++ src/components/CharacterDetailsPage/ComicsList/ComicsList.tsx
@@ -3,16 +3,25 @@ import {
 	Image,
 } from '@/contexts/CharactersContext/types/characterTypes'
 import { useCharacters } from '@/contexts/CharactersContext/hooks/useCharacters'
+import { isNotEmptyArray } from '@/contexts/CharactersContext/utils/predicates'
+import { useMemo } from 'react'
 import ComicCard from '../ComicCard/ComicCard'
 import styles from './ComicsList.module.scss'
 
 const ComicsList = ({ comics }: { comics: ComicsImages | undefined }) => {
 	const { isLoadingImages } = useCharacters()
 	const showPlaceholder = (isLoadingImages && !comics) || !comics
-
+	const hasImages = useMemo(
+		() => comics && isNotEmptyArray(comics?.images),
+		[comics]
+	)
 	return (
 		<div className={styles.comics_list}>
 			<h1>Comics</h1>
+			{!hasImages && !showPlaceholder && (
+				<h2 style={{ overflowX: 'hidden' }}>No comics found</h2>
+			)}
+
 			<ul>
 				{showPlaceholder
 					? [...Array(5)].map(index => <ComicCard key={index} />)
